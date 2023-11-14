58544011c2eb16b6b593f7c1592f0d49f10ca273
Denys Dashkevych <denysdashkevych@gmail.com>
2023-11-14 17:42:13 -0600

    update

--- lesson_1/lesson1.py
+++ lesson_1/lesson1.py
@@ -5,3 +5,22 @@ Created on Wed Nov  1 19:17:24 2023
 
 @author: prestige
 """
+
+def fibonacci_dynamic_programming(n):
+    # Создаем массив для сохранения промежуточных результатов
+    fib_array = [0] * (n + 1)
+
+    # Известные значения для базовых случаев
+    fib_array[0] = 0
+    fib_array[1] = 1
+
+    # Заполняем массив значениями Фибоначчи от 2 до n
+    for i in range(2, n + 1):
+        fib_array[i] = fib_array[i - 1] + fib_array[i - 2]
+
+    return fib_array[n]
+
+
+n = 10
+result = fibonacci_dynamic_programming(n)
+print(f"Число Фибоначчи для n={n}: {result}")
