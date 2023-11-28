9c7d6c63446f614180bc5a3b17fcbcf2d8a296b9
Denys Dashkevych <majorprestige99@gmail.com>
2023-11-27 20:50:40 -0600

    Create exam.py

---
+++ lesson_3/exam.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 19:05:39 2023
+
+@author: prestige
+"""
+
+"""
+
+songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)] and max_size = 12.2, the function will return ['Roar','Wannabe','Timber']
+songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)] and max_size = 11, the function will return ['Roar','Wannabe']
+
+"""
+
+from collections import OrderedDict
+
+def song_playlist(songs, max_size):
+    """
+    songs: list of tuples, ('song_name', song_len, song_size)
+    max_size: float, maximum size of total songs that you can fit
+
+    Start with the song first in the 'songs' list, then pick the next 
+    song to be the one with the lowest file size not already picked, repeat
+
+    Returns: a list of a subset of songs fitting in 'max_size' in the order 
+             in which they were chosen.
+    """
+    playlist = []
+    first_song = songs[0]
+    first_song_name = first_song[0]
+    first_song_size = first_song[2]
+    
+    if first_song_size > max_size:
+        return []
+    
+    playlist.append(first_song_name)
+    updated_size = max_size - first_song_size
+    
+
+    next_songs = songs[1:]
+    next_songs.sort(key=lambda x: x[2])
+    
+    for song in next_songs:
+        song_name, song_len, song_size = song
+        
+        if song_size <= updated_size:
+            playlist.append(song_name)
+            updated_size -= song_size
+        
+
+    
+    return playlist
+    
+
+    
+    
+    
+    
+    
+# songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)]
+# songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)]
+
+# print(song_playlist(songs, 12.2))
+
+
+
+
+#%%
+
+import random
+random.seed(3)
+  
+# You are given this function - do not modify
+def createRandomGraph():
+    """Creates a digraph with 7 randomly chosen integer nodes from 0 to 9 and
+    randomly chosen directed edges (between 10 and 20 edges)
+    """
+    g = {}
+    n = random.sample([0,1,2,3,4,5,6,7,8,9], 7)
+    for i in n:
+        g[i] = []
+    edges = random.randint(10,20)
+    count = 0
+    while count < edges:
+        a = random.choice(n)
+        b = random.choice(n)
+        if b not in g[a] and a != b:
+            g[a].append(b)
+            count += 1
+    return g
+
+g = createRandomGraph()
+# print(g)
+
+# You are given this function - do not modify
+def findPath(g, start, end, path=[]):
+    """ Uses DFS to find a path between a start and an end node in g.
+    If no path is found, returns None. If a path is found, returns the
+    list of nodes """
+    path = path + [start]
+    if start == end:
+        return path
+    if not start in g:
+        return None
+    for node in g[start]:
+        if node not in path:
+            newpath = findPath(g, node, end, path)
+            if newpath: return newpath
+    return None
+
+# print(findPath(g, 3, 8))
+                
+#########################        
+## WRITE THIS FUNCTION ##
+#########################        
+
+def allReachable(g, n):
+    """
+    Assumes g is a directed graph and n a node in g.
+    Returns a sorted list (increasing by node number) containing all 
+    nodes m such that there is a path from n to m in g. 
+    Does not include the node itself.
+    """
+    #TODO
+
+
+def allReachable(g, n):
+    """
+    Assumes g is a directed graph and n a node in g.
+    Returns a sorted list (increasing by node number) containing all 
+    nodes m such that there is a path from n to m in g. 
+    Does not include the node itself.
+    """
+    def dfs(node, visited):
+        """
+        Helper function for depth-first search.
+        """
+        visited.add(node)
+        reachable_nodes = {node}
+        for neighbor in g.get(node, []):
+            if neighbor not in visited:
+                reachable_nodes.update(dfs(neighbor, visited))
+        return reachable_nodes
+
+    visited_nodes = set()
+    reachable_nodes = dfs(n, visited_nodes)
+    
+    # Sort the result and remove the starting node n
+    result = sorted(list(reachable_nodes - {n}))
+    return result
+
+# Example usage:
+# Assuming g is the directed graph you created earlier with createRandomGraph()
+start_node = random.choice(list(g.keys()))
+reachable_nodes_from_start = allReachable(g, start_node)
+print(f"Nodes reachable from node {start_node}: {reachable_nodes_from_start}")
+
+#%%
+
+
+def solveit(test):
+    """ test, a function that takes an int parameter and returns a Boolean
+        Assumes there exists an int, x, such that test(x) is True
+        Returns an int, x, with the smallest absolute value such that test(x) is True 
+        In case of ties, return any one of them. 
+    """
+    # IMPLEMENT THIS FUNCTION
+
+#### This test case prints 49 ####
+def f(x):
+    return (x+15)**0.5 + x**0.5 == 15
+print(solveit(f))
+
+#### This test case prints 0 ####
+def f(x):
+    return x == 0
+print(solveit(f))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
