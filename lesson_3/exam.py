aed33de79b014b3b9c0b6a392d83e4a3de170b38
Denys Dashkevych <majorprestige99@gmail.com>
2023-11-27 21:15:44 -0600

    summary

--- lesson_3/exam.py
+++ lesson_3/exam.py
@@ -165,16 +165,25 @@ def solveit(test):
         Returns an int, x, with the smallest absolute value such that test(x) is True 
         In case of ties, return any one of them. 
     """
-    # IMPLEMENT THIS FUNCTION
+    
+    try:
+        for i in range(-1000, 1000):
+            
+            if test(i):
+                if test(0):
+                    return 0
+                if test(-i):
+                    return -i
+                return i
+            else:
+                i += 1
+    except IndexError:
+        return 3
+        
 
 #### This test case prints 49 ####
 def f(x):
-    return (x+15)**0.5 + x**0.5 == 15
-print(solveit(f))
-
-#### This test case prints 0 ####
-def f(x):
-    return x == 0
+    return [1,2,3][-x] == 1 and x != 0
 print(solveit(f))
 
 
--- lesson_3/lectures_notes.py
+++ lesson_3/lectures_notes.py
@@ -55,3 +55,33 @@ plt.xlabel('Час між дефектами (години)')
 plt.ylabel('Щільність ймовірності')
 plt.legend()
 plt.show()
+
+
+#%%
+
+import random
+        
+def G():  
+    random.seed(0)
+    mylist = []
+    r = 1
+
+    if random.random() > 0.99:
+        r = random.randint(1, 10)
+    for i in range(r):
+        if random.randint(1, 10) > 3:
+            number = random.randint(1, 10)
+            mylist.append(number)
+            print(mylist)
+    
+    
+print(G())
+
+
+
+
+
+
+
+
+
