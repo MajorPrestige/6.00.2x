acd57d276f6e02f1af91999a2111b07737c9c3a9
Denys Dashkevych <majorprestige99@gmail.com>
2023-12-15 20:24:43 -0600

    Update ps4.py

--- lesson_4/pset4/ps4.py
+++ lesson_4/pset4/ps4.py
@@ -233,3 +233,273 @@ evaluate_models_on_training(x1, y, models)
 # # MISSING LINES
 # models = generate_models(x, y, [1])    
 # evaluate_models_on_training(x, y, models)
+
+#%%
+
+
+import random
+
+class Lecture(object):
+    def __init__(self, listen, sleep, fb):
+        self.listen = listen
+        self.sleep = sleep
+        self.fb = fb
+    def get_listen_prob(self):
+        return self.listen
+    def get_sleep_prob(self):
+        return self.sleep
+    def get_fb_prob(self):
+        return self.fb
+     
+def get_mean_and_std(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**0.5
+    return mean, std
+        
+def lecture_activities(N, aLecture):
+    '''
+    N: integer, number of trials to run
+    aLecture: Lecture object
+ 
+    Runs a Monte Carlo simulation N times.
+    Returns: a tuple, (float, float)
+              Where the first float represents the mean number of lectures it takes 
+              to have a lecture in which all 3 activities take place,
+              And the second float represents the total width of the 95% confidence 
+              interval around that mean.
+    '''
+    # IMPLEMENT THIS FUNCTION
+
+    tot = []
+    
+    for _ in range(N):
+        lect = 0
+        act = set()
+        while len(act) < 3:
+            if random.uniform(0, 1) < aLecture.get_listen_prob():
+                act.add('listen')
+            if random.uniform(0, 1) < aLecture.get_sleep_prob():
+                act.add('sleep')
+            if random.uniform(0, 1) < aLecture.get_fb_prob():
+                act.add('fb')
+            lect += 1
+        tot.append(lect)
+
+    mean, std = get_mean_and_std(tot)
+    total_width = 1.96 * std
+    
+    # Calculate margin of error using the t-distribution critical value
+    margin_of_error = 1.96 * std * mean
+
+    return mean, margin_of_error
+
+    # return mean, total_width
+
+
+# sample test cases 
+a = Lecture(1, 1, 1)
+print(lecture_activities(100, a))
+# the above test should print out (1.0, 0.0)
+          
+b = Lecture(1, 1, 0.5)
+print(lecture_activities(100000, b))
+# # the above test should print out something reasonably close to (2.0, 5.516)
+
+
+
+
+
+#%%
+
+import random, pylab
+
+# You are given this function
+def getMeanAndStd(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**0.5
+    return mean, std
+
+# You are given this class
+class Die(object):
+    def __init__(self, valList):
+        """ valList is not empty """
+        self.possibleVals = valList[:]
+    def roll(self):
+        return random.choice(self.possibleVals)
+
+# Implement this -- Coding Part 1 of 2
+def makeHistogram(values, numBins, xLabel, yLabel, title=None):
+    """
+      - values, a sequence of numbers
+      - numBins, a positive int
+      - xLabel, yLabel, title, are strings
+      - Produces a histogram of values with numBins bins and the indicated labels
+        for the x and y axis
+      - If title is provided by caller, puts that title on the figure and otherwise
+        does not title the figure
+    """
+    # TODO
+    
+                    
+# Implement this -- Coding Part 2 of 2
+def getAverage(die, numRolls, numTrials):
+    """
+      - die, a Die
+      - numRolls, numTrials, are positive ints
+      - Calculates the expected mean value of the longest run of a number
+        over numTrials runs of numRolls rolls.
+      - Calls makeHistogram to produce a histogram of the longest runs for all
+        the trials. There should be 10 bins in the histogram
+      - Choose appropriate labels for the x and y axes.
+      - Returns the mean calculated
+    """
+    # TODO
+    
+# One test case
+print(getAverage(Die([1,2,3,4,5,6,6,6,7]), 500, 10000))
+
+#%%
+
+import random
+import pylab
+
+# You are given this function
+def getMeanAndStd(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**0.5
+    return mean, std
+
+# You are given this class
+class Die(object):
+    def __init__(self, valList):
+        """ valList is not empty """
+        self.possibleVals = valList[:]
+    def roll(self):
+        return random.choice(self.possibleVals)
+
+# Implement this -- Coding Part 1 of 2
+def makeHistogram(values, numBins, xLabel, yLabel, title=None):
+    """
+      - values, a sequence of numbers
+      - numBins, a positive int
+      - xLabel, yLabel, title, are strings
+      - Produces a histogram of values with numBins bins and the indicated labels
+        for the x and y axis
+      - If title is provided by caller, puts that title on the figure and otherwise
+        does not title the figure
+    """
+    pylab.hist(values, bins=numBins)
+    pylab.xlabel(xLabel)
+    pylab.ylabel(yLabel)
+    if title is not None:
+        pylab.title(title)
+    pylab.show()
+
+# Implement this -- Coding Part 2 of 2
+def getAverage(die, numRolls, numTrials):
+    """
+      - die, a Die
+      - numRolls, numTrials, are positive ints
+      - Calculates the expected mean value of the longest run of a number
+        over numTrials runs of numRolls rolls.
+      - Calls makeHistogram to produce a histogram of the longest runs for all
+        the trials. There should be 10 bins in the histogram
+      - Choose appropriate labels for the x and y axes.
+      - Returns the mean calculated
+    """
+    longest_runs = []
+
+    for _ in range(numTrials):
+        rolls = [die.roll() for _ in range(numRolls)]
+
+        # Calculate the longest run
+        longest_run = 1
+        current_run = 1
+        for i in range(1, numRolls):
+            if rolls[i] == rolls[i - 1]:
+                current_run += 1
+                longest_run = max(longest_run, current_run)
+            else:
+                current_run = 1
+
+        longest_runs.append(longest_run)
+
+    makeHistogram(longest_runs, 10, 'Longest Run', 'Frequency', 'Longest Run Histogram')
+
+    mean, _ = getMeanAndStd(longest_runs)
+    return mean
+
+# One test case
+print(getAverage(Die([1, 2, 3, 4, 5, 6, 6, 6, 7]), 500, 10000))
+
+
+
+
+
+
+
+
+#%%
+
+import itertools
+import numpy as np
+
+def find_combination(choices, total):
+    """
+    choices: a non-empty list of ints
+    total: a positive int
+
+    Returns result, a numpy.array of length len(choices) 
+    such that
+        * each element of result is 0 or 1
+        * sum(result*choices) == total
+        * sum(result) is as small as possible
+    In case of ties, returns any result that works.
+    If there is no result that gives the exact total, 
+    pick the one that gives sum(result*choices) closest 
+    to total without going over.
+    """
+    min_sum = float('inf')  # Initialize with positive infinity
+    best_combination = None
+
+    for r in range(1, len(choices) + 1):
+        for combination in itertools.combinations(choices, r):
+            current_sum = sum(combination)
+            if current_sum == total:
+                result = np.array([1 if choice in combination else 0 for choice in choices])
+                return result
+            elif current_sum < total and current_sum < min_sum:
+                min_sum = current_sum
+                best_combination = combination
+
+    if best_combination is not None:
+        result = np.array([1 if choice in best_combination else 0 for choice in choices])
+        return result
+    else:
+        # No exact match, return the closest result
+        result = np.array([1 if choice <= total else 0 for choice in choices])
+        return result
+
+# Example usage:
+choices = [1, 2, 2, 3]
+total = 4
+result = find_combination(choices, total)
+print(result)
+
+
+
+
+
+
+
+
+
