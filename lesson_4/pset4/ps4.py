670c8267d0acaf7acb994569f052a30cc872f2d0
Denys Dashkevych <majorprestige99@gmail.com>
2023-12-25 21:17:21 -0600

    fizzbuzz

--- lesson_4/pset4/ps4.py
+++ lesson_4/pset4/ps4.py
@@ -524,10 +524,25 @@ def find_combination(choices, total):
         return get_result(best)
     else:
         return get_result([choice for choice in choices if choice <= total])
+    
+    
+#%%
+
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz')        
+        elif i % 3 == 0:
+            print('Fizz')
+            
+        elif i % 5 == 0:
+            print("Buzz")    
+        else:
+            print(i)
 
 
 
-
+fizzBuzz(15)
 
 
 
